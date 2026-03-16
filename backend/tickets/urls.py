c64e73e93a2a0032f20c6e608a0004ee1e2b842d
Satyam Behera <118427525+SatyamBehera23@users.noreply.github.com>
2026-03-16 20:54:04 +0530

    Add API routes

---
+++ backend/tickets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (
+    TicketListCreateView,
+    TicketUpdateView,
+    TicketStatsView,
+)
+
+urlpatterns = [
+    path("tickets/", TicketListCreateView.as_view()),
+    path("tickets/<int:pk>/", TicketUpdateView.as_view()),
+    path("tickets/stats/", TicketStatsView.as_view()),
+]
