c0498b68173c613f0ef2c4532da095e30cf571c9
Satyam Behera <118427525+SatyamBehera23@users.noreply.github.com>
2026-03-16 20:50:40 +0530

    Add ticket list, create, update APIs with filtering and search

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics,filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Ticket
+from .serializers import TicketSerializer
+
+
+class TicketListCreateView(generics.ListCreateAPIView):
+
+    queryset = Ticket.objects.all().order_by("-created_at")
+    serializer_class = TicketSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+
+    filterset_fields = ["category", "priority", "status"]
+    search_fields = ["title", "description"]
+
+
+class TicketUpdateView(generics.UpdateAPIView):
+
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
